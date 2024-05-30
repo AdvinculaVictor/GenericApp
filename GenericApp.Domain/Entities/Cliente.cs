50c90bd40489c9c66c8f4aa65d184bb3c9318a3e
Victor Aguilar Advincula <victor_advincula@outlook.com>
2024-05-30 17:25:59 -0600

    change: Cliente Telefono data type change to string

--- GenericApp.Domain/Entities/Cliente.cs
+++ GenericApp.Domain/Entities/Cliente.cs
@@ -17,7 +17,7 @@ namespace GenericApp.Domain.Entities
         public string? Municipio { get; set; }
         public string? Estado { get; set; }
         public int CP { get; set; }
-        public int Telefono { get; set; }
+        public string? Telefono { get; set; }
         public string? Email { get; set; }
         public string? RFC { get; set; }
         public List<Pedido>? Pedidos { get; set; }
